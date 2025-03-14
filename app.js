d4c0c181d89b4c2a69060b9b674e3e4730dc4086
Kutullo Letageng <108078889+Kutullo20@users.noreply.github.com>
2025-03-14 03:55:49 +0200

    Add files via upload
    Final-Changes-in the main branch

---
+++ app.js
@@ -0,0 +1,10 @@
+// HAMBURGER MENU TOGGLE START
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+// HAMBURGER MENU TOGGLE END
+
+
